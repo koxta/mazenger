6f75cefa02e14752fb4aab8214d881fbb3e6ddb0
Giorgi Maziashvili <gmaziashvili@live.com>
2020-01-22 16:59:14 +0400

    test basic send and recieve

--- src/Screens/ChatScreen/ChatScreen.tsx
+++ src/Screens/ChatScreen/ChatScreen.tsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { 
   View,
   Text,
   StyleSheet,
 } from 'react-native';
+import Firebase from '../../Services/Firebase';
+import { Message, User } from '../../Models';
 
 export default () => {
+
+  useEffect(()=>{
+    Firebase.getMessages((message:Message)=>{
+      console.log(message);
+    });
+  },[])
+  
+  const buttonPress = () => {
+    const user = new User();
+    user.userName = 'gmaziashvili';
+    user.uid='qergeaerg342qqe4ga';
+    Firebase.send('hi',user);
+  }
+
   return(
     <View style={styles.container}>
       <Text>{'Chat Screen'}</Text>

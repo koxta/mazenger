9b507ca00c80c9d1f3154fd1ca4989730c6b4a0b
Giorgi Maziashvili <gmaziashvili@live.com>
2020-01-22 20:41:37 +0400

    detect if message was sent by this user

--- src/Screens/ChatScreen/components/Messages.tsx
+++ src/Screens/ChatScreen/components/Messages.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useContext, useState } from 'react';
-import { 
+import {
   View, StyleSheet, Text, FlatList
 } from 'react-native';
 import { User } from '../../../Models';
@@ -8,60 +8,69 @@ import { Store } from '../../../Store/Store';
 
 export default () => {
   const { state } = useContext(Store);
-  const { messages } = state;
-  const [messageData,setMessageData] = useState([]);
-  useEffect(()=>{
+  const { messages, user } = state;
+  const [messageData, setMessageData] = useState([]);
+  useEffect(() => {
     console.log(messages)
     setMessageData(messages)
-  },[messages])
+  }, [messages])
 
-  return(
+
+
+  return (
     <View style={styles.container}>
       <FlatList
         style={{
-          transform:[
-            {scaleY:-1}
+          transform: [
+            { scaleY: -1 }
           ]
-        }}        
+        }}
         data={messageData}
-        renderItem={(item) => <MessageComponent text={item.item.message} userName={item.item.userName}/>}
+        renderItem={(item) => <MessageComponent text={item.item.message} userName={item.item.userName} thisUser={item.item.uid == user.uid} />}
       />
     </View>
   )
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    flexDirection:'column-reverse',
+  container: {
+    flex: 1,
+    flexDirection: 'column-reverse',
   }
 })
 
 
-const MessageComponent = ({text,userName}) => {
-  return(
+const MessageComponent = ({ text, userName, thisUser }) => {
+  const position = thisUser ? 'flex-end' : 'flex-start';
+  const color = thisUser ? Colors.Primary : Colors.PrimaryLight
+
+  return (
     <View style={{
-      margin:10,
-      borderColor:Colors.Text,
-      borderWidth:2,
-      borderRadius:5,
-      alignSelf:'baseline',
-      padding:5,
-      transform:[
-        {scaleY:-1}
-      ]
+      margin: 10,
+      borderColor: Colors.Text,
+      borderWidth: 2,
+      borderRadius: 5,
+      alignSelf: position,
+      padding: 5,
+      transform: [
+        { scaleY: -1 }
+      ],
+      backgroundColor:color,
+      flexDirection:'row-reverse'
+
     }}>
       <Text style={{
-        position:'absolute',
-        top:-10,
-        left:-10,
-        color:Colors.Text,
-        backgroundColor:Colors.Primary,
-        paddingHorizontal:5
+        position: 'absolute',
+        top: -10,
+        left: -10,
+        color: Colors.Text,
+        backgroundColor: Colors.Primary,
+        paddingHorizontal: 5,
+        borderRadius:10
       }}>{userName}</Text>
       <Text style={{
-        color:Colors.Text,
-        fontSize:20
+        color: Colors.Text,
+        fontSize: 20
       }}>{text}</Text>
     </View>
   )
